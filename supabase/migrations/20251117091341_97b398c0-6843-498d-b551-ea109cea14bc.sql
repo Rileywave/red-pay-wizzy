764746d4a4150ff74c755234ac0972c779a05f19
Rileywave <rileyfreeman.xo@gmail.com>
2026-08-18 11:53:13 +0100

    commit

--- supabase/migrations/20251117091341_97b398c0-6843-498d-b551-ea109cea14bc.sql
+++
@@ -1,5 +0,0 @@
--- Create trigger to execute referral bonus function when a new referral is inserted
-CREATE TRIGGER on_referral_inserted
-  AFTER INSERT ON public.referrals
-  FOR EACH ROW
-  EXECUTE FUNCTION public.handle_new_referral();
